775ca9efbe5104f3f18d322c503e7cc7b5d94be2
Xuehai Zhou <zhxh@Xuehais-MacBook-Pro.local>
2020-04-09 13:34:51 -0700

    lab2 finished

--- labs/lab2/main.c
+++ labs/lab2/main.c
@@ -1,26 +1,48 @@
+/*
+* Description: from the stdin load the input. Then splits the inputs by the space delimiter 
+*
+* Author: Xuehai Zhou
+*
+* Date: Apr 9, 2020
+*
+* Notes: 
+*/
+
+/*-------------------------Preprocessor Directives---------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/*---------------------------------------------------------------------------*/
 
-int main()
-{
+/*-----------------------------Program Main----------------------------------*/
+int main() {
+	setbuf(stdout, NULL);
+	/* Main Function Variables */
 	FILE *fp = stdin;
 	char *text = NULL;
 	char *text_cp = NULL;
 	size_t len = 0; 
 	char *token = NULL;
-	char delimiter = ' ';
 	char exit[5] = "exit";
+	
+	/* Allocate memory for the input buffer. */
+	char* input = (char*)malloc(sizeof(char)*BUFSIZ);
+	
+	/*main run loop*/
 	while (1) { 
+		/* Print >>> then get the input string */
 		printf(">>> ");
 		getline(&text, &len, fp);
 		text[strlen(text) - 1] = '\0';
 		if (strlen(text) > 0) printf("\n");
+		/* If the user entered <exit> then exit the loop */
 		if (strcmp(text, exit) == 0) break;
 		else {
 			int i = 0;
 			text_cp = text;
+			/* Tokenize the input string */
 			token = strtok_r(text_cp, " ", &text_cp);
+			/* Display each token */
 			while (token != NULL) {
 				printf("T%d: %s\n", i, token);
 				i++;
@@ -28,6 +50,10 @@ int main()
 			}
 		}
 	}
+	/*Free the allocated memory*/
+	free(input);
 	free(text);
-	return 0;
+
+	return 1;
 }
+/*-----------------------------Program End-----------------------------------*/
--- labs/lab2/main2.c
+++ labs/lab2/main2.c
@@ -1,26 +1,48 @@
+/*
+* Description: read a file from argv. Then splits words or characters by the space delimiter
+*
+* Author: Xuehai Zhou
+*
+* Date: Apr 9, 2020
+*
+* Notes: 
+*/
+
+/*-------------------------Preprocessor Directives---------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/*---------------------------------------------------------------------------*/
 
-int main(int argc, char const *argv[])
-{
+/*-----------------------------Program Main----------------------------------*/
+int main(int argc, char const *argv[]) {
+	setbuf(stdout, NULL);
+	/* Main Function Variables */
 	FILE *fp = fopen(argv[1], "r");
 	char *text = NULL;
 	char *text_cp = NULL;
 	size_t len = 0; 
-	ssize_t nread;
-	char *token = NULL;
-	char delimiter = ' ';
+	char *token = "NULL";
 	char exit[5] = "exit";
-	while (getline(&text, &len, fp) != -1) { 
+	
+	/* Allocate memory for the input buffer. */
+	char* input = (char*)malloc(sizeof(char)*BUFSIZ);
+	
+	/*main run loop*/
+	while (token != NULL) { 
+		/* Print >>> then get the input string */
 		//printf(">>> ");
+		getline(&text, &len, fp);
 		text[strlen(text) - 1] = '\0';
 		//if (strlen(text) > 0) printf("\n");
+		/* If the user entered <exit> then exit the loop */
 		if (strcmp(text, exit) == 0) break;
 		else {
 			int i = 0;
 			text_cp = text;
+			/* Tokenize the input string */
 			token = strtok_r(text_cp, " ", &text_cp);
+			/* Display each token */
 			while (token != NULL) {
 				printf("T%d: %s\n", i, token);
 				i++;
@@ -28,8 +50,10 @@ int main(int argc, char const *argv[])
 			}
 		}
 	}
+	/*Free the allocated memory*/
+	free(input);
 	free(text);
-	fclose(fp);
-	return 0;
-}
 
+	return 1;
+}
+/*-----------------------------Program End-----------------------------------*/
