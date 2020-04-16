606ec669a838832a14a45cb1e30357df54a4a156
Xuehai Zhou <zhxh@Xuehais-MacBook-Pro.local>
2020-04-15 22:26:49 -0700

    lfcat and output.txt difference

---
+++ labs/lab3/command.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "command.h"
+
+
+void lfcat()
+{
+	// Define your variables here
+	FILE *fp;
+	DIR *dir;
+	//int fnamelen;
+	size_t len = 1024;
+	char *currDir;
+	static char *buffer;
+	struct dirent *sd;
+
+	// memory allcation
+	char *line = (char *)malloc(len * sizeof(char));
+	if (line == NULL) {
+		fprintf(stderr, "line allocation failure\n");
+		exit(1);
+	}
+
+	// Get the current directory
+	currDir = getcwd(buffer, len);
+	//printf("current working directory: %s\n", currDir);
+	
+	// Open the dir using opendir()
+	dir = opendir(currDir);
+	if (dir == NULL) {
+        fprintf(stderr, "Error! Unable to open current directory.\n");
+        exit(EXIT_FAILURE);
+    }
+
+	// use a while loop to read the dir
+	while ((sd=readdir(dir)) != NULL) {
+		if (strcmp(sd->d_name, ".") != 0 && strcmp(sd->d_name, "..") != 0) {
+			printf("%s\n", sd->d_name);
+		}
+		// Hint: use an if statement to skip any names that are not readable files (e.g. ".", "..", "main.c", "a.out", "output.txt"
+		if (strstr(sd->d_name, ".c") == NULL
+            && strstr(sd->d_name, ".o") == NULL
+            && strstr(sd->d_name, ".h") == NULL
+            && strstr(sd->d_name, ".pdf") == NULL
+            && strstr(sd->d_name, ".exe") == NULL
+            && strcmp(sd->d_name, "output.txt")
+            && strcmp(sd->d_name, "input.txt")
+            && strcmp(sd->d_name, "Makefile")
+            && strcmp(sd->d_name, ".DS_Store"))  {
+			// Open the file
+			//fnamelen = strlen(sd->d_name);
+	        write(1, "File: ", 6);
+	        write(1, sd->d_name, strlen(sd->d_name));
+	        write(1, "\n", 1);
+			fp = fopen(sd->d_name, "r");
+			// Read in each line using getline()
+			while (getline(&line, &len, fp) != -1) {
+				// Write the line to stdout
+				write(1, line, strlen(line));
+			}
+			write(1, "\n", 1);
+			// write 80 "-" characters to stdout
+			for (int i = 0; i < 80; ++i) {
+				printf("-");
+			}
+			write(1, "\n", 1);
+		}
+	}
+	// free memory 
+	free(line);
+
+	// close the read file and free/null assign your line buffer
+	fclose(fp);
+
+	//close the directory you were reading from using closedir()
+	closedir(dir);
+}
