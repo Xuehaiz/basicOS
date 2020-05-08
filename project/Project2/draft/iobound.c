fff45a198aa2a615025e64c525e9bcd4f018d418
Xuehai Zhou <zhxh@Xuehais-MacBook-Pro.local>
2020-05-08 09:37:38 -0700

    part3 finished

--- project/Project2/draft/iobound.c
+++ project/Project2/draft/iobound.c
@@ -1,10 +1,15 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-    long long i;
-    int minutes = 1, j;
+    int i, j, minutes = 1, number = 600000000;
+    char b[1000];
+    int fd = open("/dev/null", O_WRONLY);
     char name[128];
 
 /*
@@ -22,10 +27,9 @@ int main(int argc, char **argv) {
             exit(1);
         }
     }
-    for (j = 0; j < minutes; j++) {
-        for (i = 0; i < 3000000000; i++) {
-            ;
-        }
+    for (i = 0; i < minutes; i++) {
+        for (j = 0; j < number; j++)
+             (void) write(fd, b, sizeof(b));
     }
     return 0;
 }
--- project/Project2/part3.c
+++ project/Project2/part3.c
@@ -9,144 +9,157 @@
 #include <unistd.h>
 
 
-/*void sig_handler(int sig, siginfo_t *siginfo, void *context) {  // 
-	printf("SIGNAL RECEIVED!\n");
-
-	sigset_t sigset;
-  	sigemptyset(&sigset);
-  	sigaddset(&sigset, SIGUSR1);
-  	sigprocmask(SIG_BLOCK, &sigset, NULL);
-
-  	// unblock if block
-  	if (sigwait(&sigset, &sig) == 0) {
-  		printf("Unblock here\n");
-  	}
-}*/
-
-
-void sig_handler(int sig) {  // 
-	printf("Child process: %d - recieved signal: %d", getpid(), sig);
-
-  	// unblock if block
-  	/*if (sigwait(&sigset, &sig) == 0) {
-  		printf("Unblock here\n");
-  	}*/
+void sig_handler(int sig) {  
+    printf("Child process: %d - received signal: %d\n", getpid(), sig);
 }
 
 int main(int argc __attribute__((unused)), char const *argv[])
 {
-	// Variable declarations
-	int condition = 1;
-	char *token;
-	int len = 256; 
-	char line[len];
-	int counter = 0;
-	int i = 0;
-	int numprograms = 0;
-	// open file
-	FILE *fp = fopen(argv[1], "r");
-	if (fp == NULL) {
-		fprintf(stderr, "File <%s> open failure.\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	// memory allcation
-	char **arg_arr = (char **)malloc(len * sizeof(char *));
-	if (arg_arr == NULL) {
-		fprintf(stderr, "Buffer allocation failure\n");
-		exit(EXIT_FAILURE);
-	}
-	pid_t *pid = (pid_t *)malloc(len * sizeof(pid_t));
-	if (pid == NULL) {
-		fprintf(stderr, "pid list allocation failure\n");
-		exit(EXIT_FAILURE);
-	}
+    // Variable declarations
+    char *token;
+    int len = 256; 
+    char line[len];
+    int counter = 0;
+    int i = 0;
+    int numprograms = 0;
+    // open file
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        fprintf(stderr, "File <%s> open failure.\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    // memory allcation
+    char **arg_arr = (char **)malloc(len * sizeof(char *));
+    if (arg_arr == NULL) {
+        fprintf(stderr, "Buffer allocation failure\n");
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+    pid_t *pid = (pid_t *)malloc(len * sizeof(pid_t));
+    if (pid == NULL) {
+        fprintf(stderr, "pid list allocation failure\n");
+        fclose(fp);
+        free(arg_arr);
+        exit(EXIT_FAILURE);
+    }
 
-	// sig struct and handler
-	int status;
-	sigset_t sigset;
-  	sigemptyset(&sigset);
-  	sigaddset(&sigset, SIGUSR1);
-  	sigprocmask(SIG_BLOCK, &sigset, NULL);
-	struct sigaction sa;
-	if (memset (&sa, '\0', sizeof(sa)) == NULL) {
-		perror("memset");
-	}
-	sa.sa_handler = &sig_handler;
-	if (sigaction(SIGUSR1, &sa, NULL) == -1) {
-		perror("SIGUSR1");
-	}
-	if (sigaction(SIGCONT, &sa, NULL) == -1) {
-		perror("SIGCONT");
-	}
+    // sig struct and handler
+    // int status;
+    sigset_t sigset;
+    sigemptyset(&sigset);
+    sigaddset(&sigset, SIGALRM);
+    sigprocmask(SIG_BLOCK, &sigset, NULL);
+    struct sigaction sa;
+    if (memset (&sa, '\0', sizeof(sa)) == NULL) {
+        perror("memset");
+    }
+    sa.sa_handler = &sig_handler;
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        perror("SIGUSR1");
+    }
+    /*if (sigaction(SIGSTOP, &sa, NULL) == -1) {
+        perror("SIGSTOP");
+    }*/
+    if (sigaction(SIGCONT, &sa, NULL) == -1) {
+        perror("SIGCONT");
+    }
 
-	while (fgets(line, len, fp) != NULL) {
-		numprograms++;
-		counter = 0;
-		token = strtok(line, " \n");
-		while (token != NULL) {
-			arg_arr[counter] = token;
-			token = strtok(NULL, " \n");
-			counter++;
-		}
-		arg_arr[counter] = NULL;
+    while (fgets(line, len, fp) != NULL) {
+        numprograms++;
+        counter = 0;
+        token = strtok(line, " \n");
+        while (token != NULL) {
+            arg_arr[counter] = token;
+            token = strtok(NULL, " \n");
+            counter++;
+        }
+        arg_arr[counter] = NULL;
 
-		pid[i] = fork();
-		if (pid[i] < 0) {
-			perror("fork error, no child created");
-			fclose(fp);
-			exit(EXIT_FAILURE);
-		}
+        pid[i] = fork();
+        if (pid[i] < 0) {
+            perror("fork error, no child created");
+            fclose(fp);
+            free(pid);
+            free(arg_arr);
+            exit(EXIT_FAILURE);
+        }
+        if (pid[i] == 0) {
+            printf("Child process: %d - Starting executing %s.\n", getpid(), arg_arr[0]);
+            // raise SIGSTOP
+            printf("Child process: %d - rasing SIGSTOP\n", getpid());
+            raise(SIGSTOP);
+            // Exec call
+            printf("Child process: %d - calling exec().\n", getpid());
+            execvp(arg_arr[0], arg_arr);
+            // error report and free 
+            perror("execvp");
+            fclose(fp);
+            free(pid);
+            free(arg_arr);
 
-		// send signal to pid[i] to stop cihld
-		// kill(pid[i], SIGUSR1);
+            // End the child process
+            _exit(-1);
+        }
+        i++;
+    }
+    
+    int condition = 1;
+    int one = 1;
+    int alive = 0;
+    int signal;
+    int status;
 
-		if (pid[i] == 0) {
-    		printf("Child process: %d - Starting execution.\n", getpid());
-		    // Waiting for SIGUSR1
-		    printf("Child process: %d - Waiting for SIGALRM...\n", getpid());
-		    int signal = SIGALRM;
-		    if (sigwait(&sigset, &signal) == 0) {
-		    	printf("Child process: %d - Received signal: SIGUSR1 - calling exec().\n", getpid());
-		    }
-		    // Exec call
-		    execvp(arg_arr[0], arg_arr);
-		    // error report and free 
-		    perror("execvp");
-		    fclose(fp);
-			free(pid);
-			free(arg_arr);
-		    // End the child process
-		    _exit(-1);
-		}
-		else {
-		    //Send SIGUSR1 to the children
-		    printf("Parent process: %d - Sending signal: %d to child process: %d\n", getpid(), SIGUSR1, pid[i]);
-		    kill(pid[i], SIGUSR1);
-		    printf("Parent process: %d - Signal SIGUSR1 sent.\n", getpid());
-  		}
-		i++;
-	}
-	
-	for (int i = 0; i < numprograms; i++) {
-		waitpid(pid[i], NULL, 0);
-		// wait(0);
-		printf("wait pid[%d]: %d\n", i, pid[i]);
-	}
-	for (int i = 0; i < numprograms; i++) {
-		printf("Sending signal: %d to pid: %d\n", SIGSTOP, pid[i]);
-		kill(pid[i], SIGSTOP);
-		printf("Signal SIGSTOP sent.\n");
-	}
-	for (int i = 0; i < numprograms; i++) {
-		printf("Sending signal: %d to pid: %d\n", SIGCONT, pid[i]);
-		kill(pid[i], SIGCONT);
-		printf("Signal SIGCONT sent.\n");
-	}
+    // waitpid(-1, NULL, WNOHANG);
+    while (condition) {
+        condition = 0;
+        for (int k = 0; k < numprograms; k++) {
+            if (waitpid(pid[k], &status, WNOHANG) != -1) {  // determine if the process is alive
+                kill(pid[k], SIGCONT);
+                alarm(1);
+                printf("Parent PID %d resumed suspended child PID %d\n", getpid(), pid[k]);
+                if (sigwait(&sigset, &signal) == 0) {
+                    printf("Child process: %d - Received signal: SIGALRM\n", pid[k]);
+                }
+                if (waitpid(pid[k], &status, WNOHANG) != -1) {
+                    kill(pid[k], SIGSTOP);
+                    printf("Parent PID %d suspended unfinished child PID %d\n", getpid(), pid[k]);
+                    condition = 1; 
+                }
+                printf("curr: %d\n", k);
+            }
+            // count the number of the finished processes
+            alive = 0;
+            for (int i = 0; i < numprograms; i++) {
+                if (waitpid(pid[i], &status, WNOHANG) != -1) {
+                    alive++;
+                }
+            }
+            printf("alive: %d\n", alive);
+            if (alive <= 1) {
+                // run the last process 
+                for (int i = 0; i < numprograms; i++) {
+                    if (waitpid(pid[i], &status, WNOHANG) == 0) {
+                        kill(pid[i], SIGCONT);
+                        printf("Finishing up the last process: %d\n", pid[i]);
+                    }
+                }
+                condition = 0; 
+                break;
+            }
+        }
+    }
+    
+    for (int i = 0; i < numprograms; i++) {
+        waitpid(pid[i], NULL, 0);
+        // wait(0);
+        printf("wait pid[%d]: %d\n", i, pid[i]);
+    }
 
-	// exit all
-	printf("All processes finished: parent exiting: my pid is %d \n\n", getpid());
-	fclose(fp);
-	free(pid);
-	free(arg_arr);
-	return 0;
+    // exit all
+
+    printf("All processes finished: parent exiting: my pid is %d \n\n", getpid());
+    fclose(fp);
+    free(pid);
+    free(arg_arr);
+    return 0;
 }
